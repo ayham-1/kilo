0c7823ad1c66f06b51eee37d20f4f57c304ee0bd
Ayham Mamoun <ayhamaboualfadl@gmail.com>
2019-02-01 10:47:13 +0300

    Chap2 Done

---
+++ CMakeFiles/kilo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kilo.dir/kilo/kilo.c.o"
+  "bin/kilo.pdb"
+  "bin/kilo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kilo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
